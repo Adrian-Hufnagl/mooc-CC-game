0902c3d955a3f1b1cb646ea5e1a4ecf1df528702
adrianhufnagl <adrianhufnagl@gmx.net>
2022-05-05 19:34:43 +0200

    random cards and role selection

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var explainer = false;
+
+function switchStage() {
+  var stage1 = document.getElementById("stage-1");
+  var stage2 = document.getElementById("stage-2");
+
+  if (stage1.style.display === "none") {
+    stage1.style.display = "block";
+  } else {
+    stage1.style.display = "none";
+  }
+  if (stage2.style.display === "none") {
+    stage2.style.display = "block";
+  } else {
+    stage2.style.display = "none";
+  }
+}
+
+function shuffleCards() {
+  var cards1 = document.querySelector('#card-list-1');
+  var cards2 = document.querySelector('#card-list-2');
+  for (var i = cards1.children.length; i >= 0; i--) {
+    cards1.appendChild(cards1.children[Math.random() * i | 0]);
+  }
+  for (var i = cards2.children.length; i >= 0; i--) {
+    cards2.appendChild(cards2.children[Math.random() * i | 0]);
+  }
+}
+
+function startGame(creator) {
+  if(!creator){
+    explainer = true;
+  }
+  var stage1 = document.getElementById("stage-1");
+  var start = document.getElementById("start");
+  stage1.style.display = "block";
+  start.style.display = "none";
+
+}
